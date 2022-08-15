7d4daaf4644aee342558fb851f2b10f93591c5bc
Brian Morgan <brianwmorgan317@gmail.com>
2022-08-15 18:37:39 -0400

    modified all files with solution code as directed in lesson 47.7

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -1,5 +1,9 @@
 const LinkedList = require("../lib/linkedList");
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// ITERATIVE SOLUTION //
+
 /**
  * Iteratively reverse a linked list
  *
@@ -9,7 +13,34 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
+
+// PSEUDOCODE //
+
+//  function reverseIterative(list)
+//  // accepts list - a linked list, potentially empty, to be reversed
+
+//  declare a variable named reversedList and initialize it to an empty LinkedList
+//  declare a variable node initialized to the head of the list
+//  while the node pointer is not null do
+//    insert the node value at the head of the reversedList
+//    move the node pointer along to the next node
+// return the reversedList
+
+// JAVASCRIPT IMPLEMENTATION //
+
+function reverseIterative(list) {
+  const reversedList = new LinkedList();
+  let node = list.head;
+  while (node) {
+    reversedList.insertAtHead(node.value);
+    node = node.next;
+  }
+  return reversedList;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// RECURSIVE SOLUTION //
 
 /**
  * Recursively reverse a linked list
@@ -20,6 +51,20 @@ function reverseIterative(list) {}
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseRecursive(list) {}
+function reverseRecursive(list) {
+  if (list.length <= 1) {
+    return list;
+  }
+
+  const head = list.head.value;
+  list.remove((node, index) => index === 0);
+  const reversedList = reverseRecursive(list);
+  reversedList.insert(head);
+  return reversedList;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// EXPORT //
 
 module.exports = { reverseIterative, reverseRecursive };
